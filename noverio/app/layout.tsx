1b3880eaaff6e5c4f56338f985b44c4c0f6c4ba7
yagami-24 <noverio2499@gmail.com>
2025-07-15 08:31:13 +0000

    fix error any

--- noverio/app/layout.tsx
+++ noverio/app/layout.tsx
@@ -20,7 +20,7 @@ export const metadata = {
   description: "",
 };
 
-export default function RootLayout({ children }: {children: any}) {
+export default function RootLayout({ children }: {children: React.ReactNode}) {
   return (
     <html lang="en" suppressHydrationWarning className="scroll-smooth">
       <body
